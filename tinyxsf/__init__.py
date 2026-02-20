db0e0c57103dca63f0c6148a6fd6185d50fbf328
Johannes Buchner <johannes.buchner.acad@gmx.com>
2026-02-20 14:13:01 +0100

    Bump version: 1.1.0 → 1.2.0

--- tinyxsf/__init__.py
+++ tinyxsf/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Johannes Buchner"""
 __email__ = 'johannes.buchner.acad@gmx.com'
-__version__ = '1.1.0'
+__version__ = '1.2.0'
 
 from .data import load_pha
 from .model import (FixedTable, Table, logNegBinomialPDF, logPoissonPDF,
